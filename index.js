2f2933007dff5f1ee46ba198559e8625a55a2425
aha00a <aha00a@gmail.com>
2019-12-24 02:11:02 +0900

    apply async

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const deepdash = require('deepdash/standalone'); // full
 
-console.log('Hello, schema.org!');
-console.log(deepdash);
+(async () => {
+    console.log('Hello, schema.org!');
+    console.log(deepdash);
+})();
