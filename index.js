a789f7f160bc5e2abefb32caa664809f5aefd2bd
aha00a <aha00a@gmail.com>
2019-12-25 21:29:45 +0900

    implement

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const deepdash = require('deepdash/standalone'); // full
+const fse = require('fs-extra');
+const mapRecursive = require('map-recursive');
+
+const traversePath = async (path, callback) => {
+    const stat = await fse.stat(path);
+    if (!stat.isDirectory()) {
+        callback.onFile(path);
+        return path;
+    }
+    callback.onDir(path);
+    const files = await fse.readdir(path);
+    return files.map(async v => await traversePath(`${path}/${v}`, callback))
+};
 
 (async () => {
-    console.log('Hello, schema.org!');
-    console.log(deepdash);
+    await fse.ensureDir('transformed');
+
+    const convertObjectOnlyHaveAtIdToString = v => {
+        if (typeof v !== 'object')
+            return v;
+
+        const keys = Object.keys(v);
+        if (keys.length !== 1)
+            return v;
+
+        if (keys[0] !== '@id')
+            return v;
+
+        return v['@id'];
+    };
+
+    function convertPath(f) {
+        return f.replace(/^original/, 'transformed');
+    }
+
+    await traversePath('original', {
+        onDir: async f => {
+            await fse.ensureDir(convertPath(f));
+        },
+        onFile: async f => {
+            const content = await fse.readFile(f, 'utf-8');
+            var v = mapRecursive.mapRecursive(
+                JSON.parse(content),
+                convertObjectOnlyHaveAtIdToString
+            );
+            const json = JSON.stringify(v, null, 4);
+            await fse.writeFile(convertPath(f), json, 'utf-8')
+        }
+    });
 })();
