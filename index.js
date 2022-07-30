5266060fdfb9c4c15816a7761fe6d3daeb3791a1
aha00a <aha00a@gmail.com>
2022-07-31 04:55:22 +0900

    support v14

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const traversePath = async (path, callback) => {
         onDir: async f => await fse.ensureDir(convertPath(f)),
         onFile: async f => {
             const content = await fse.readFile(f, 'utf-8');
+            console.log(f);
 
             const c1 = mapRecursive.mapRecursive(JSON.parse(content), v => {
                 if (typeof v !== 'object')
@@ -37,19 +38,44 @@ const traversePath = async (path, callback) => {
 
                 return v['@id'];
             });
-            const c2 = mapRecursive.mapRecursiveKey( c1, v => v.replace(/^(@|rdf:|rdfs:|schema:|http:\/\/schema.org\/)/, ""));
-            const c3 = mapRecursive.mapRecursiveLeaf(c2, v => v.replace(/^(@|rdf:|rdfs:|schema:|http:\/\/schema.org\/)/, ""));
+            const c2 = mapRecursive.mapRecursiveKey( c1, v => v.replace?.(/^(@|rdf:|rdfs:|schema:|http:\/\/schema.org\/)/, "") ?? v);
+            const c3 = mapRecursive.mapRecursiveLeaf(c2, v => v.replace?.(/^(@|rdf:|rdfs:|schema:|http:\/\/schema.org\/)/, "") ?? v);
             const json = JSON.stringify(c3, null, 4);
             return await fse.writeFile(convertPath(f), json, 'utf-8')
         }
     });
 
-    const text = await fse.readFile('transformed/5.0/tree.jsonld', 'utf-8');
-    const tree = JSON.parse(text);
-    const result = JSON.parse(JSON.stringify(mapRecursive.mapRecursiveLeaf(
-        tree,
-        (v, k) => ['id', 'children'].includes(k) ? v : undefined
-    )));
-    await fse.writeFile('transformed/5.0/tree.pruned.jsonld', JSON.stringify(result, null, 4), 'utf-8')
+    [
+        {
+            pathTree: 'transformed/5.0/tree.jsonld',
+            pathTreePruned: 'transformed/5.0/tree.pruned.jsonld',
+        },
+        {
+            pathTree: 'transformed/14.0/tree.jsonld',
+            pathTreePruned: 'transformed/14.0/tree.pruned.jsonld',
+        },
+    ].reduce(async (a, v) => {
+        await a;
+        const {pathTree, pathTreePruned } = v;
 
+        const text = await fse.readFile(pathTree, 'utf-8');
+        const tree = JSON.parse(text);
+        const treePruned = mapRecursive.mapRecursiveLeaf(
+            tree,
+            (v, k) => ['id', 'children'].includes(k) ? v : undefined
+        );
+        const filtered = mapRecursive.mapRecursive(treePruned, v => {
+            if(typeof v !== 'object')
+                return v;
+
+            if(Array.isArray(v))
+                return v;
+
+            Object.keys(v).filter(k => !['id', 'children'].includes(k)).map(k => delete v[k]); // TODO: need to be pure function
+            return v;
+        });
+        const result = JSON.parse(JSON.stringify(filtered));
+        await fse.writeFile(pathTreePruned, JSON.stringify(result, null, 4), 'utf-8')
+        return [];
+    }, [])
 })();
